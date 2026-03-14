cca460faa9521ddcb8af5ae7e2350905fa271af5
TrejoUaemex <89501593+TrejoUaemex@users.noreply.github.com>
2026-03-13 18:02:04 -0600

    Add files via upload

---
+++ tema2/Ejercicio_Operadores_Arit.py
@@ -0,0 +1,9 @@
+def calcular_calorias (Carbohidratos,proteinas,grasas):
+    Total1=Carbohidratos*4
+    Total2=proteinas*4
+    Total3=grasas*9
+    Total_calorias=Total1+Total2+Total3
+    print (f"Calorias totales : {Total_calorias}")
+
+
+calcular_calorias(10,40,5) 
